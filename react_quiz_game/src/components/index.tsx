f25f51ea5bbe8b08cefb8d64eaac9ded65c6a0c3
suwanshree <suwanshree@gmail.com>
2022-11-03 16:42:07 -0700

    Questions display on question card

---
+++ react_quiz_game/src/components/index.tsx
@@ -0,0 +1,2 @@
+export { default as Questioncard } from './Questioncard/index';
+export { default as Appbutton } from './Button/index';
--- react_quiz_game/src/pages/homescreen/index.tsx
+++ react_quiz_game/src/pages/homescreen/index.tsx
@@ -1,6 +1,36 @@
+import { useState, useEffect } from "react";
+import { Difficulty, totalQuestions } from '@/constants/index';
+import { getQuestionList } from '@/services/fetchQuestions';
+import { Questioncard } from '@/components/index';
+import { QuestionProps } from '@/interface/index';
+
 const Homepage = () => {
+    const [questions, setQuestions] = useState<QuestionProps[]>([]);
+    const [questionNumber, setQuestionNumber] = useState(0);
+
+    useEffect (() => {
+        const fetchQuestions = async () => {
+            const questionListing = await getQuestionList(
+                totalQuestions, Difficulty.HARD
+            );
+            setQuestions(questionListing);
+        }
+        fetchQuestions();
+    },[])
+
+    const checkAnswer = () => {
+        console.log("check answer")
+    }
+
     return (
-        <div><h1>Homepage</h1></div>
+        <div>
+            <Questioncard questions={questions[questionNumber].question}
+            category={questions[questionNumber].category}
+            callback={checkAnswer}
+            totalQuestions={totalQuestions}
+            questionNumber={questionNumber}
+            />
+        </div>
     )
 }
 
